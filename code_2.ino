d1d7e64d0e32d45ef206266e4c2a1111ddf930e7
Anpoke <47451687+Anpoke@users.noreply.github.com>
2022-09-29 12:46:50 +0200

    Add files via upload

--- code_2.ino
+++ code_2.ino
@@ -7,7 +7,7 @@
 #define   RESET           0x01
 
 // CR95HF: echo
-#define   ECHO            0x55
+//#define   ECHO            0x55
 
 //Pin definition
 const int SPI_SS = 10;
@@ -27,11 +27,11 @@ void PowerUp(){
   digitalWrite(8,HIGH);  
 }
 
-void ECHOResp(){
+void ECHO(){
   //SEND to CR95HF
   digitalWrite(SPI_SS, LOW);
   SPI.transfer(SEND);
-  SPI.transfer(ECHO);
+  SPI.transfer(0x55);
   digitalWrite(SPI_SS, HIGH);
 
   //READ from CR95HF
@@ -40,14 +40,48 @@ void ECHOResp(){
   SPI.transfer(READ);
   r = SPI.transfer(0x00);
   digitalWrite(SPI_SS, HIGH);
-  Serial.print("r égal :");
+  Serial.print("r = ");
   Serial.print(r, HEX);
-  Serial.print("\n");
+  Serial.println();
+  
+}
+
+void IDN(){
+  //SEND to CR95HF
+  digitalWrite(SPI_SS, LOW);
+  SPI.transfer(SEND);
+  SPI.transfer(0x01);
+  SPI.transfer(0x00);
+  digitalWrite(SPI_SS, HIGH);
+
+  //POLL CR95HF
+  digitalWrite(SPI_SS, LOW);
+  SPI.transfer(POLL);
+  byte flag = 0x00; // Initialise la boucle
+  //Test du bit n°3 de MISO
+  while(bitRead(flag,3)!=1){
+    flag = SPI.transfer(POLL);
+    delay(100);
+    }
+  digitalWrite(SPI_SS, HIGH);
+
+  //READ from CR95HF
+  Serial.print("IDN = ");
+  digitalWrite(SPI_SS, LOW);
+  SPI.transfer(READ);
+  byte resp = SPI.transfer(0x00);
+  byte len = SPI.transfer(0x00);
+  for (int i = 0; i <= len; i++) {
+    byte data = SPI.transfer(0x00);
+    Serial.print(char(data));
+  }
+    Serial.println();
+  digitalWrite(SPI_SS, HIGH);
   
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(500000);
 
   pinMode(SPI_SS, OUTPUT);
   pinMode(IRQ_IN, OUTPUT);
@@ -59,9 +93,12 @@ void setup() {
   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
 
   PowerUp();
+  ECHO();
+  IDN();
 }
 
 void loop() {
 
-  ECHOResp();
+  //ECHO();
+  
 }
